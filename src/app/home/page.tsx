33d812f07c2364b63ac0ccf38facfe9a519b5204
Yash Raj <kyashraj991@gmail.com>
2025-06-11 15:16:51 +0530

    feat: add DateTime component and update globals.css for font integration

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import DateTime from '@/components/DateTime';
 import { ModeToggle } from '@/components/ModeToggle';
 import { Target } from 'lucide-react';
 import React from 'react';
@@ -15,10 +16,8 @@ const page = () => {
         </div>
       </div>
 
-      <div className="bg-gray-700 border ">
-        <div className="bg-blue-950 py-20  items-center justify-center flex">
-          <h1 className='text-4xl font-bold text-yello-300 text-shadow-2xs font-edu'>Good Morning, James!</h1>
-        </div>
+      <div>
+        <DateTime />
       </div>
     </div>
   );
---
+++ src/components/DateTime.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+const DateTime = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formattedDate = time.toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+
+  const dayName = time
+    .toLocaleDateString('en-US', { weekday: 'long' })
+    .toUpperCase();
+
+  return (
+    <div className="bg-blue-600 text-shadow-2xs text-white border py-4  gap-4 flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-semibold text-shadow-2xs font-edu">
+        Good Morning, James!
+      </h1>
+      <div className=" flex flex-col font-osw gap-4 justify-center items-center">
+        <p className="text-7xl md:text-9xl ">
+          {' '}
+          {time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+        </p>
+        <p className="text-2xl">{formattedDate}</p>
+        <p className="text-4xl">{dayName}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DateTime;
