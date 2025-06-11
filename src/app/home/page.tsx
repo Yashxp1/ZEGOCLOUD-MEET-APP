c629451fab4c7ba531669b78c3df0859b5392eb8
Yash Raj <kyashraj991@gmail.com>
2025-06-11 13:44:49 +0530

    shadcn setup

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from '@/components/ModeToggle';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default page;
