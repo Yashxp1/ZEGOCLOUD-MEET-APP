cf2e8b09757e432d5a6e9a80537ed862fe4139f4
Yash Raj <kyashraj991@gmail.com>
2025-06-11 14:22:10 +0530

    feat: add Radix UI components and implement sidebar functionality
    - Added @radix-ui/react-dialog, @radix-ui/react-separator, and @radix-ui/react-tooltip dependencies.
    - Created Input, Separator, Sheet, and Sidebar components for UI structure.
    - Implemented Sidebar context for managing open/closed state and mobile responsiveness.
    - Enhanced home page layout with a header and greeting message.
    - Introduced Skeleton and Tooltip components for loading states and user guidance.
    - Added useIsMobile hook to detect mobile view for responsive design.
    - Updated global styles to include new fonts and improved layout.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,25 @@
 import { ModeToggle } from '@/components/ModeToggle';
+import { Target } from 'lucide-react';
 import React from 'react';
 
 const page = () => {
   return (
-    <div>
-      <ModeToggle />
+    <div className="border rounded-md">
+      <div className="border px-4 py-2 rounded-md h-fit w-full flex justify-between items-center">
+        <div className="flex items-center gap-2">
+          <Target size={32} />
+          <span className="font-semibold">Chat Wave</span>
+        </div>
+        <div>
+          <ModeToggle />
+        </div>
+      </div>
+
+      <div className="bg-gray-700 border ">
+        <div className="bg-blue-950 py-20  items-center justify-center flex">
+          <h1 className='text-4xl font-bold text-yello-300 text-shadow-2xs font-edu'>Good Morning, James!</h1>
+        </div>
+      </div>
     </div>
   );
 };
