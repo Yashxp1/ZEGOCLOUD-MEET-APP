1e2dd40d824ec2bc5aac287b3ec7fed3fe0f06d0
Yash Raj <kyashraj991@gmail.com>
2025-06-12 19:23:48 +0530

    feat: implement user registration API with email and username uniqueness checks

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,56 @@
+import prisma from '@/lib/prisma';
+import { hash } from 'bcrypt';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email, username, password } = body;
+
+    const existingUserByEmail = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: 'user with this email already exits',
+        },
+        { status: 409 }
+      );
+    }
+
+    const existingUserByUsername = await prisma.user.findUnique({
+      where: { username: username },
+    });
+
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: 'user with this username already exits',
+        },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json({
+      user: newUser,
+      message: 'User created successfully',
+    });
+  } catch (error) {
+    console.error('Error registering the user', error);
+    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+  }
+}
