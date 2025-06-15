cabe537178aaaa7f3717a2e09d907b2ccefe4585
Yash Raj <kyashraj991@gmail.com>
2025-06-15 15:27:12 +0530

    feat: add user session management with logout functionality

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,23 @@
 import DateTime from '@/components/DateTime';
+import Link from 'next/link';
 import MeetingBox from '@/components/MeetingBox';
 import { ModeToggle } from '@/components/ModeToggle';
+import { authOptions } from '@/lib/auth';
 import { Target } from 'lucide-react';
+import { getServerSession } from 'next-auth';
 import React from 'react';
+import { Button, buttonVariants } from '@/components/ui/button';
+import UserLogOut from '@/components/UserLogOut';
 
-const page = () => {
+const page = async () => {
+  const session = await getServerSession(authOptions);
+
+  console.log(session);
+
   return (
     <div className="border">
       <div className="border px-4 py-2 h-fit w-full flex justify-between items-center">
+        <div className="bg-purple-600">SESSION:{session?.user.username}</div>
         <div className="flex items-center gap-2">
           <Target size={32} />
           <span className="font-semibold">Chat Wave</span>
@@ -20,8 +30,20 @@ const page = () => {
       <div>
         <DateTime />
       </div>
-      <div className=''>
-        <MeetingBox/>
+      <div className="">
+        <MeetingBox />
+      </div>
+      <div>
+        {/* <Link>
+        </Link> */}
+
+        {session?.user ? (
+          <UserLogOut />
+        ) : (
+          <Link className={buttonVariants()} href="/login">
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -51,6 +51,29 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      
+      if (user) {
+        return {
+          ...token,
+          username: user.username,
+        };
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          username: token.username,
+        },
+      };
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
