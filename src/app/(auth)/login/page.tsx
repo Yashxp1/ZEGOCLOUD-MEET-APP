d58a9bba6239b15d2597c69663e1c5bf7f143c11
Yash Raj <kyashraj991@gmail.com>
2025-06-17 11:23:21 +0530

    Revert login and register pages to commit 8e9051c

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,86 +1,40 @@
-'use client';
-
 import Link from 'next/link';
-import React, { useState } from 'react';
-import toast from 'react-hot-toast';
-import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import React from 'react';
 
 const page = () => {
-  const [form, setForm] = useState({ email: '', password: '' });
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-
- const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
-  const onSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setLoading(true);
-
-    const signInData = await signIn('credentials', {
-       redirect: false,
-      email: form.email,
-      password: form.password,
-    });
-    console.log(signInData);
-    if (signInData?.error) {
-      toast.error(signInData.error);
-    } else {
-      toast.success('Logged in successfully');
-      router.push('/home');
-    }
-
-    setLoading(false);
-  };
-
   return (
-    <form onSubmit={onSubmit}>
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="flex gap-4 flex-col justify-center items-center">
-          <div>
-            <p className="py-4 text-xl font-semibold">
-              Login in with your email{' '}
-            </p>
-          </div>
-          <div className="flex flex-col md:w-96 w-72 gap-4">
-            <input
-              placeholder="Email"
-              type="email"
-              name='email'
-              value={form.email}
-              onChange={handleChange}
-              required
-              className="dark:bg-[#1E1E1E] font-semibold outline-0 bg-gray-200 px-2 py-3 rounded-lg"
-            />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex gap-4 flex-col justify-center items-center">
+        <div>
+          <p className="py-4 text-xl font-semibold">
+            Login in with your email{' '}
+          </p>
+        </div>
+        <div className="flex flex-col md:w-96 w-72 gap-4">
+          <input
+            placeholder="Email"
+            type="text"
+            className="dark:bg-[#1E1E1E] font-semibold outline-0 bg-gray-200 px-2 py-3 rounded-lg"
+          />
 
-            <input
-              placeholder="Password"
-              name='password'
-              type="password"
-              value={form.password}
-              required
-              onChange={handleChange}
-              className="dark:bg-[#1E1E1E] font-semibold bg-gray-200 px-2 py-3 rounded-lg outline-0"
-            />
-          </div>
-          <div className="flex flex-col gap-3">
-            <button
-              type="submit"
-              className=" bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100"
-            >
-              {loading ? 'Logging in...' : 'Login'}
+          <input
+            placeholder="Password"
+            type="password"
+            className="dark:bg-[#1E1E1E] font-semibold bg-gray-200 px-2 py-3 rounded-lg outline-0"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <button className=" bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100">
+            Login
+          </button>
+          <Link href={'/register'}>
+            <button className=" bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100">
+              Register
             </button>
-            <Link href={'/register'}>
-              <button className=" bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100">
-                Register
-              </button>
-            </Link>
-          </div>
+          </Link>
         </div>
       </div>
-    </form>
+    </div>
   );
 };
 
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,104 +1,46 @@
-'use client';
-
-import toast, { Toaster } from 'react-hot-toast';
 import Link from 'next/link';
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import React from 'react';
 
-const RegisterPage = () => {
-  const router = useRouter()
-  const [form, setForm] = useState({ username: '', email: '', password: '' });
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setLoading(true);
-
-    try {
-      const res = await fetch('/api/user', {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify(form),
-      });
-
-      const data = await res.json();
-      if (!res.ok) {
-        throw new Error(data.message);
-      } else {
-        router.push('/login');
-      }
-
-      toast.success(data.message || 'User created successfully!');
-      setForm({ email: '', username: '', password: '' });
-    } catch (error: any) {
-      console.log(error);
-      toast.error(error?.message || 'Registration failed');
-    } finally {
-      setLoading(false);
-    }
-  };
-
+const page = () => {
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="flex gap-4 flex-col justify-center items-center">
-          <p className="py-4 text-xl font-semibold">Register</p>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex gap-4 flex-col justify-center items-center">
+        <div>
+          <p className="py-4 text-xl font-semibold">
+            Login in with your email{' '}
+          </p>
+        </div>
+        <div className="flex flex-col md:w-96 w-72 gap-4">
+          <input
+            placeholder="Name"
+            type="text"
+            className="dark:bg-[#1E1E1E] font-semibold outline-0 bg-gray-200 px-2 py-3 rounded-lg"
+          />
+          <input
+            placeholder="Email"
+            type="email"
+            className="dark:bg-[#1E1E1E] font-semibold outline-0 bg-gray-200 px-2 py-3 rounded-lg"
+          />
 
-          <div className="flex flex-col md:w-96 w-72 gap-4">
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={form.email}
-              onChange={handleChange}
-              required
-              className="dark:bg-[#1E1E1E] font-semibold outline-0 bg-gray-200 px-2 py-3 rounded-lg"
-            />
-            <input
-              type="text"
-              name="username"
-              placeholder="Username"
-              value={form.username}
-              onChange={handleChange}
-              required
-              className="dark:bg-[#1E1E1E] font-semibold outline-0 bg-gray-200 px-2 py-3 rounded-lg"
-            />
-            <input
-              type="text"
-              name="password"
-              placeholder="Password"
-              value={form.password}
-              onChange={handleChange}
-              required
-              className="dark:bg-[#1E1E1E] font-semibold bg-gray-200 px-2 py-3 rounded-lg outline-0"
-            />
-          </div>
-
-          <div className="flex flex-col gap-3">
-            <button
-              type="submit"
-              disabled={loading}
-              className="bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100"
-            >
-              {loading ? 'Registering...' : 'Register'}
+          <input
+            placeholder="Password"
+            type="password"
+            className="dark:bg-[#1E1E1E] font-semibold bg-gray-200 px-2 py-3 rounded-lg outline-0"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <button className=" bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100">
+            Register
+          </button>
+          <Link href="/login">
+            <button className=" bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100">
+              Login
             </button>
-            <Link href="/login">
-              <button className="bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100">
-                Login
-              </button>
-            </Link>
-          </div>
+          </Link>
         </div>
       </div>
-      <Toaster position="top-center" reverseOrder={false} />
-    </form>
+    </div>
   );
 };
 
-export default RegisterPage;
+export default page;
