5a23d20d347f5ca543bec91e19a824b03ee3661f
Yash Raj <kyashraj991@gmail.com>
2025-06-21 10:45:07 +0530

    fix: correct link destination from login to register

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -28,7 +28,7 @@ const page = () => {
             Login
           </button>
 
-          <Link href="/login">
+          <Link href="/register">
             <button className=" bg-white transition-colors border border-[#D5D5D5] dark:border-[#2D2D2D] text-black py-2 md:w-96 w-72 rounded-lg font-semibold hover:bg-gray-100">
               Register
             </button>
