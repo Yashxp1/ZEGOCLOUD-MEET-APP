cf53bd0623231be7aa828ac3e1e93005dc906869
Yash Raj <kyashraj991@gmail.com>
2025-06-23 17:21:09 +0530

    feat: enhance authentication middleware to handle private and auth routes redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,9 +12,25 @@ export default auth(async (req) => {
   const isLoggedIn = !!req.auth;
   const { nextUrl } = req;
 
+  const url = 'http://localhost:3000';
+
   const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
   const isAuthRoute = nextUrl.pathname.includes('/auth');
   const isApiRoute = nextUrl.pathname.includes('/api');
+
+  if (isApiRoute) return;
+
+  if (isLoggedIn && isAuthRoute) {
+    return Response.redirect(`${url}/home`);
+  }
+
+  if (isAuthRoute && !isLoggedIn) {
+    return;
+  }
+
+  if (!isLoggedIn && isPrivateRoute) {
+    return Response.redirect(`${url}/auth/login`);
+  }
 });
 
 export const config = {
