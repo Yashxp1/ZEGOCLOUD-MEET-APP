8cc39e8f45d2600e46a213d612fd653c86387dbb
Yash Raj <kyashraj991@gmail.com>
2025-06-24 21:12:10 +0530

    feat: enhance home and landing pages with improved layout and navigation

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,5 @@
+import SignOut from '@/components/auth/SignOut';
+import MeetBtn from '@/components/MeetBtn';
 import { ModeToggle } from '@/components/ModeToggle';
 import { auth } from '@/lib/auth';
 import { Target } from 'lucide-react';
@@ -7,13 +9,17 @@ const page = async () => {
   const session = await auth();
   console.log(session);
   return (
-    <div className="border flex items-center justify-between px-4 py-2">
-      <div className="flex items-center gap-2">
+    <div className="">
+      <div className="gap-2   border flex items-center justify-between px-4 py-2">
         <Target size={32} />
         <span className="font-semibold">Chat Wave</span>
+        <div className="flex gap-4">
+          <ModeToggle />
+          <SignOut />
+        </div>
       </div>
       <div>
-        <ModeToggle />
+        <MeetBtn />
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import React from 'react';
 
 const page = () => {
   return (
-    <div className='text-8xl'>LANDING PAGE</div>
-  )
-}
+    <div className="text-8xl">
+      LANDING PAGE
+      <Link href="/home">
+        <div>
+          <Button className="bg-green-500">Go to home</Button>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
-export default page
+export default page;
