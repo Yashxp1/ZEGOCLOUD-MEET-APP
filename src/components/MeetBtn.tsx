b5a1b9c166b8155c65366400939f7ff320d67f08
Yash Raj <kyashraj991@gmail.com>
2025-06-25 19:12:31 +0530

    feat: add session joing button and input box

--- src/components/MeetBtn.tsx
+++ src/components/MeetBtn.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Button } from './ui/button';
-import { Video } from 'lucide-react';
+import { Plus } from 'lucide-react';
+import { Input } from './ui/input';
 
 const MeetBtn = () => {
   return (
-    <div>
+    <div className="border flex flex-col gap-4 p-4 justify-center items-center">
       <Button className="bg-blue-600 text-white hover:bg-blue-500">
-        <p>New meeting</p> <Video />
+        <p>New Session</p> <Plus />
       </Button>
+      <div className='flex gap-3'>
+        <Input placeholder="Enter session code" className="" />
+        <Button>Join</Button>
+      </div>
     </div>
   );
 };
