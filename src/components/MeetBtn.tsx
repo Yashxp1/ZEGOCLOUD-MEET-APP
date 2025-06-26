5e9cb7b1d9e8c85807657a52960a3cae01a3451f
Yash Raj <kyashraj991@gmail.com>
2025-06-26 21:36:05 +0530

    feat: implement Agora RTC logic for real-time communication

--- src/components/MeetBtn.tsx
+++ src/components/MeetBtn.tsx
@@ -6,9 +6,9 @@ import { Input } from './ui/input';
 const MeetBtn = () => {
   return (
     <div className="border flex flex-col gap-4 p-4 justify-center items-center">
-      <Button className="bg-blue-600 text-white hover:bg-blue-500">
+      {/* <Button className="bg-blue-600 text-white hover:bg-blue-500">
         <p>New Session</p> <Plus />
-      </Button>
+      </Button> */}
       <div className='flex gap-3'>
         <Input placeholder="Enter session code" className="" />
         <Button>Join</Button>
