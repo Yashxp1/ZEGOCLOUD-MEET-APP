68f2632321d691d92c65dad6fa4769d09fabddc0
Yash Raj <kyashraj991@gmail.com>
2025-06-27 20:01:47 +0530

    refactor: remove Agora RTC logic and voiceLink component, update package dependencies

--- src/agoraSDK/voiceLink.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const voiceLink = () => {
-  return (
-    <div>voiceLink</div>
-  )
-}
-
-export default voiceLink
--- src/app/home/[slug]/page.tsx
+++ src/app/home/[slug]/page.tsx
@@ -5,77 +5,11 @@ import { User } from 'lucide-react';
 import { useParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
-import AgoraRTC from 'agora-rtc-sdk-ng';
 
 const page = () => {
   const params = useParams();
   const router = useRouter();
   const slug = params.slug;
-
-  // ----------------------AGORA RTC LOGIC ---------------------------
-
-  const appId = process.env.AGORA_APPId as string;
-  const token = process.env.AGORA_TOKEN as string;
-
-  const [userId, setUserId] = useState(null);
-
-  const [RTCuid, setRTCuid] = useState<number>();
-
-  const rtcUid = Math.floor(Math.random() * 1000);
-  console.log(rtcUid);
-
-  const roomId = slug as string;
-
-  let rtcClient;
-
-  let audioTracks: {
-    localAudioTrack: any;
-    remoteAudioTracks: { [key: string]: any };
-  } = {
-    localAudioTrack: null,
-    remoteAudioTracks: {},
-  };
-
-  const handleUserJoined = async (user: any) => {
-    setUserId(user.uid);
-
-    console.log('USER -> ', user);
-    console.log('USER UID -> ', user.uid);
-  };
-
-  const handleUserPublished = async ({ user, mediaType }: any) => {
-    if (mediaType === 'audio') {
-      audioTracks.remoteAudioTracks[user.uid] = user.audioTrack;
-      user.audioTrack.play();
-    }
-  };
-
-  const handleUserLeft = async (user: any) => {
-    if (audioTracks.remoteAudioTracks[user.uid]) {
-      audioTracks.remoteAudioTracks[user.uid].stop();
-      delete audioTracks.remoteAudioTracks[user.uid];
-
-      setUserId(null);
-      router.push('/home');
-    }
-  };
-
-  const initRTC = async () => {
-    rtcClient = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
-
-    rtcClient.on('user-joined', handleUserJoined);
-    rtcClient.on('user-published', handleUserPublished);
-    rtcClient.on('user-left', handleUserLeft);
-
-    await rtcClient.join(appId, roomId, token, rtcUid);
-
-    audioTracks.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
-    await rtcClient.publish(audioTracks.localAudioTrack);
-    console.log('rtcUID -> ', rtcUid);
-    setRTCuid(rtcUid);
-  };
-
-  //------------------------------------------------------------------
 
   return (
     <div className="flex gap-4 flex-col justify-center items-center">
@@ -86,12 +20,12 @@ const page = () => {
         <Button className="bg-red-600 text-white hover:bg-red-800">
           Leave
         </Button>
-        <Button onClick={handleUserJoined}>User button</Button>
+        <Button>User button</Button>
       </div>
       <div className="flex gap-4">
         <div className="border-2 rounded-full p-10">
           <User size={64} />
-          <span>user: {userId} - {RTCuid}</span>
+          <span>userID: 123</span>
         </div>
         <div className="border-2 rounded-full p-10">
           <User size={64} />
