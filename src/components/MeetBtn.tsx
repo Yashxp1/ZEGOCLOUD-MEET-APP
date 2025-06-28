587a0e52224804b8ba31aebd41b1aebb9b9101f4
Yash Raj <kyashraj991@gmail.com>
2025-06-28 16:36:24 +0530

    feat: implement session joining functionality in MeetBtn component

--- src/components/MeetBtn.tsx
+++ src/components/MeetBtn.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+'use client';
+import React, { useRef } from 'react';
 import { Button } from './ui/button';
 import { Plus } from 'lucide-react';
 import { Input } from './ui/input';
+import { useRouter } from 'next/navigation';
 
 const MeetBtn = () => {
+  const joinRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+
+  const handleJoin = () => {
+    console.log(joinRef);
+    router.push(`/home/${joinRef.current?.value.trim()}`);
+  };
+
   return (
     <div className="border flex flex-col gap-4 p-4 justify-center items-center">
-      {/* <Button className="bg-blue-600 text-white hover:bg-blue-500">
-        <p>New Session</p> <Plus />
-      </Button> */}
-      <div className='flex gap-3'>
-        <Input placeholder="Enter session code" className="" />
-        <Button>Join</Button>
+      <div className="flex gap-3">
+        <Input ref={joinRef} placeholder="Enter session code" className="" />
+        <Button onClick={handleJoin}>Join</Button>
       </div>
     </div>
   );
