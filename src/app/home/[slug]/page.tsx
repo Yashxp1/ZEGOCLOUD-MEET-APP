90a2d6a398e7c006cfa6d2cf5a2ddba5327acf85
Yash Raj <kyashraj991@gmail.com>
2025-06-28 16:36:28 +0530

    refactor: remove unused imports from home page component

--- src/app/home/[slug]/page.tsx
+++ src/app/home/[slug]/page.tsx
@@ -1,12 +1,7 @@
 'use client';
-
-import { Button } from '@/components/ui/button';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-import { User } from 'lucide-react';
 import { useParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useRef, useState } from 'react';
-import dynamic from 'next/dynamic';
 
 const page = () => {
   const params = useParams();
@@ -16,7 +11,8 @@ const page = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   const appID = 68151614;
-  const serverSecret = process.env.NEXT_PUBLIC_ZEGOCLOUD_SERVER_SECRET as string;
+  const serverSecret = process.env
+    .NEXT_PUBLIC_ZEGOCLOUD_SERVER_SECRET as string;
 
   const myMeet = async (element: HTMLDivElement) => {
     const { ZegoUIKitPrebuilt } = await import(
@@ -33,6 +29,8 @@ const page = () => {
 
     const zp = ZegoUIKitPrebuilt.create(kitToken);
     zp.joinRoom({
+      turnOnCameraWhenJoining: false,
+      showMyCameraToggleButton: false,
       container: element,
       sharedLinks: [
         {
@@ -52,6 +50,8 @@ const page = () => {
     });
   };
 
+  const hasJoined = useRef(false);
+
   useEffect(() => {
     if (!containerRef.current) {
       console.log('container not found');
@@ -59,32 +59,9 @@ const page = () => {
     }
 
     myMeet(containerRef.current);
-  }, [slug]);
+  }, []);
 
-  return (
-    <div ref={containerRef} className="h-full w-full"></div>
-    // <div className="flex gap-4 flex-col justify-center items-center">
-    //   This is the meeting id:{' '}
-    //   <span className="px-2 bg-slate-500 rounded">{slug}</span>
-    //   <div className="flex gap-4">
-    //     <Button>Mute</Button>
-    //     <Button className="bg-red-600 text-white hover:bg-red-800">
-    //       Leave
-    //     </Button>
-    //     <Button>User button</Button>
-    //   </div>
-    //   <div className="flex gap-4">
-    //     <div className="border-2 rounded-full p-10">
-    //       <User size={64} />
-    //       <span>userID: 123</span>
-    //     </div>
-    //     <div className="border-2 rounded-full p-10">
-    //       <User size={64} />
-    //       <span>user: 123</span>
-    //     </div>
-    //   </div>
-    // </div>
-  );
+  return <div ref={containerRef} className="h-full w-full"></div>;
 };
 
 export default page;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,8 +5,6 @@ import { ModeToggle } from '@/components/ModeToggle';
 import { auth } from '@/lib/auth';
 import { Spline } from 'lucide-react';
 import React from 'react';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-
 
 const page = async () => {
   
